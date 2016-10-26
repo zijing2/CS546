cb0fb846178ed278b1b366d742014335a82b20ec
黄梓敬 <415940392@qq.com>
2016-10-25 21:41:22 -0400

    fix bug

--- lab6/routes/events.js
+++ lab6/routes/events.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const data = require("../data");
 const events = data.events;
 const people = data.people;
+const locations = data.locations;
 
 // Single Event Page
 router.get("/:id", (req, res) => {
@@ -24,7 +25,17 @@ router.get("/:id", (req, res) => {
                         }
                     }
                 }
-                res.status(200).render("events/single",  { event : event, people:peopleList } );
+                return locations.getAllLocations().then((locationsList)=>{
+                    var location;
+                    for (var i = 0; i < locationsList.length; i++) {
+                        if(locationsList[i].id == event.location){
+                            location = locationsList[i];
+                        }                        
+                    }
+                     res.status(200).render("events/single",  { event : event, people : peopleList, location : location } );
+                });
+
+               
             }).catch((e) => {
                 res.status(500).json({ error: e });
             });
@@ -39,8 +50,32 @@ router.get("/", (req, res) => {
     // Display a list of all events; it can be in an unordered list, or a table
     // Each of these events need to link to the single event page
     
+    
     return events.getAllEvents().then((eventsList)=>{
-        res.render("events/all",  { events : eventsList } );
+        return people.getAllPeople().then((peopleList)=>{            
+            return locations.getAllLocations().then((locationsList)=>{
+                for (var i = 0; i < eventsList.length; i++) {
+                    var peopleArr = [];
+                    var locationsArr = [];
+                    for (var j = 0; j < peopleList.length; j++) {
+                       if(eventsList[i].attendees.indexOf(peopleList[j].id) >= 0){
+                           peopleArr.push(peopleList[i]);
+                       }                        
+                    }
+                    for (var z = 0; z < locationsList.length; z++) {
+                        if(eventsList[i].location == locationsList[z].id){
+                            locationsArr.push(locationsList[z]);
+                        }
+                    }
+                    eventsList[i].peopleArr = peopleArr;
+                    eventsList[i].locationsArr = locationsArr;
+                }
+
+                res.render("events/all",  { events : eventsList } );
+            })
+        });
+
+        
     }).catch((e) => {
         res.status(500).json({ error: e });
     });
