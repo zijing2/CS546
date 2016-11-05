1c3afb6fbefff49bd17d8cbd12572f1c71e19f02
黄梓敬 <415940392@qq.com>
2016-11-05 16:29:29 -0400

    fix bug

--- lab7/data/form.js
+++ lab7/data/form.js
@@ -3,22 +3,26 @@ let exportedMethods = {
         return new Promise((fulfill, reject) => {
 
             if(typeof text != "string" || !(text)) {
-                throw "Must provide a text"
+                throw {name:"text",message:"Must provide a text"};
             }
             if(typeof string != "string" || !(string)) {
-                throw "Must provide a string"
+                throw {name:"string",message:"Must provide a string"};
             }
 
-            if(num1 < 1 || num1 > 25 || num2 < 1 || num2 > 25){
-                throw "That both numbers are greater than or equal to 1 and less than or equal to 25.";
+            if(num1 < 1 || num1 > 25 ){
+                throw {name:"num1", message:"That number are greater than or equal to 1 and less than or equal to 25."};
+            }
+
+            if(num2 < 1 || num2 > 25){
+                throw {name:"num2", message:"That number are greater than or equal to 1 and less than or equal to 25."};
             }
 
             num1 = parseInt(num1);
             num2 = parseInt(num2);
-            if (typeof num1 !== "number") throw "Must provide a number";
-            if (isNaN(num1)) throw "Must provide a number";
-            if (typeof num2 !== "number") throw "Must provide a number";
-            if (isNaN(num2)) throw "Must provide a number";
+            if (typeof num1 !== "number") throw {name:"num1",message:"Must provide a number"};
+            if (isNaN(num1)) throw {name:"num1",message:"Must provide a number"};
+            if (typeof num2 !== "number") throw {name:"num2",message:"Must provide a number"};
+            if (isNaN(num2)) throw {name:"num2",message:"Must provide a number"};
 
             var textLength = text.length;
             if(num1*num2 > textLength - 1){
--- lab7/routes/form.js
+++ lab7/routes/form.js
@@ -16,9 +16,21 @@ router.post("/serverform", (req, res) => {
 
     form.textManipulate(formPostData.textarea, formPostData.string, formPostData.number1, formPostData.number2)
         .then((output) => {
-            res.render("form/result", {output: output});
+            res.render("form/server", {result: output, text:formPostData.textarea, string:formPostData.string, num1:formPostData.number1, num2:formPostData.number2});
         }).catch((e) => {
-            res.render("form/server", {error: e});
+            if(e.name!="text"){
+                var text = formPostData.textarea;
+            }
+            if(e.name!="string"){
+                var string = formPostData.string;
+            }
+            if(e.name!="num1"){
+                var num1 = formPostData.number1;
+            }
+            if(e.name!="num2"){
+                var num2 = formPostData.number2;
+            }
+            res.render("form/server", {error: e, text:text, string:string, num1:num1, num2:num2});
         });
 });
 
